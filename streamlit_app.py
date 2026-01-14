6bd98061f06bedf1f5f8909c48fa6f0d473f0974
Mohd. Asaad Abrar S. <asaadsayed7@gmail.com>
2026-01-14 22:38:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Car Price Prediction", page_icon="🚗", layout="
 
 API_URL = (
     "https://car-prediction-lpfl.onrender.com/predict"
-    or "http://127.0.0.1:8000/predict"
+    or "http://127.0.0.1:8000/predict" or "https://car-prediction-lpfl.onrender.com"
 )  # change if your endpoint differs
 
 st.title("🚗 Car Price Prediction")
